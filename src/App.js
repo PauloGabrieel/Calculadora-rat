a1169476fa77c7bdd1e4611d107dd52f6b1f89d3
PauloGabrieel <gabrieeel.c@gmail.com>
2022-07-24 12:23:00 -0300

    feat: cria formulário de entrada de dados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
-import reset from "./assets/styles/reset.css"
+import styled from "styled-components";
 
 export default function App(){
     return(
-        <h1>Teste </h1>
+        <Container>
+            <Title>Calculadora de Rateio</Title>
+            <form>
+                <div>
+                    <label htmlFor="campoAtual">CUSTO ATUAL:</label>
+                    <input id="campoAtual" type="number"></input>
+                </div>
+                <div>
+                    <label htmlFor="campoStone">CUSTO STONE:</label>
+                    <input id="campoStone" type="number"></input>
+                </div>
+                <div>
+                    <label htmlFor="campoTpv">TPV:</label>
+                    <input id="campoTpv" type="number"></input>    
+                </div>
+                <div>
+                    <label htmlFor="campoSku">SKU:</label>
+                    <input id="campoSku" type="number"></input>
+                </div>
+            </form>
+            <ContainerButton>
+                <button>Calcular</button>
+                <button>Limpar</button>
+            </ContainerButton>
+        </Container>
     );
 };
+
+const Title = styled.h1`
+    font: 700 32px 'Raleway', sans-serif;
+    color: #FFFFFF;
+    margin-bottom: 30px;
+`
+const Container = styled.div`
+    padding:35px;
+    height: 100vh;
+    background-color: #45F622;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    color: #FFFFFF;
+
+    form{
+        max-width: 395px;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    };
+
+    div{
+        display: flex;
+        justify-content: space-between;
+        width: 314px;
+    }
+`;
+const ContainerButton = styled.div`
+    
+`;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import ReactDOM from "react-dom";
+
+import "./assets/styles/reset.css"
+import "./assets/styles/style.css"
 import App from "./App.js";
+
 
 ReactDOM.render(<App />, document.querySelector(".root"));
